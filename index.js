f18b629fc9b4d845f4115bd4b869b5f92dd1c895
RyanDeLong <rdelong111@gmail.com>
2022-01-27 15:42:14 -0600

    commit tree 36314b304cd77e2bb74ab5a14f5c5a6556af5081

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+class Polygon {
+	constructor(sides) {
+		this.sides = sides;
+	}
+
+	get countSides() {
+		return this.sides.length;
+	}
+
+	get perimeter() {
+		let sum = 0;
+		for (let side of this.sides) {
+			sum += side;
+		}
+		return sum;
+	}
+}
+
+class Triangle extends Polygon {
+	get isValid() {
+		const copy = this.sides.sort((a, b) => a - b);
+		if (copy[0] + copy[1] > copy[2]) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+class Square extends Polygon {
+	get isValid() {
+		for (let i = 1; i < this.sides.length; i++) {
+			if (this.sides[i - 1] !== this.sides[i]) {
+				return false
+			}
+		}
+		return true;
+	}
+
+	get area() {
+		return Math.pow(this.sides[0], 2);
+	}
+}
